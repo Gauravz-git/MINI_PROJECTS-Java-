94e7f5bc6b446dfa0b78d99df3e617d4c34f3ca9
Gaurav Chaudhari <gauravc0223@gmail.com>
2026-07-08 14:48:02 +0530

    format Main class for improved readability

--- number_format/Main.java
+++ number_format/Main.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("Enter String: ");
         Scanner sc = new Scanner(System.in);
 
@@ -13,7 +14,6 @@ public class Main {
         } catch (NumberFormatException e){
             System.out.println("input is not a valid number!!");
         }
-
         sc.close();
     }
 }
