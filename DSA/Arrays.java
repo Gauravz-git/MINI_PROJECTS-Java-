f5af6e9af50adc6611dbb6ba67b2dcc009332001
Gaurav Chaudhari <gauravc0223@gmail.com>
2026-07-26 09:00:08 +0530

    add Arrays class to demonstrate array initialization and access

---
+++ DSA/Arrays.java
@@ -0,0 +1,20 @@
+package DSA;
+
+public class Arrays {
+    public static void main(String[] args) {
+        //String[] names = {"gaurav", "rohit", "yashika"};
+        // names[0] = "vedanng";
+        // //System.out.println(names[0]);
+        // System.out.println(names.length);
+        int[] numbers = new int[5];
+
+        numbers[0] = 10;
+        numbers[1] = 20;
+        numbers[2] = 30;
+        numbers[3] = 40;
+        numbers[4] = 50;
+
+        System.out.println(numbers[0]);
+
+    }
+}
